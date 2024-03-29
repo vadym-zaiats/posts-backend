26710f68d7b2ce56c6aaf01586a0eb9f4a688711
Vadym Zaiats <vadyk.za@gmail.com>
2024-03-29 23:37:29 +0200

    - fe visual

--- client/src/assets/api/url.ts
+++
@@ -1,5 +0,0 @@
-export const URL = "http://localhost:8000/api";
-export const POSTS = `${URL}/newsposts`;
-export const REGISTER = `${URL}/auth/register`;
-export const LOGIN = `${URL}/auth/login`;
-export const USER = `${URL}/user`;
