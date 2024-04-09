dc250ce69f67005cc397808dfcf4edd9e32c7b88
Vadym Zaiats <vadyk.za@gmail.com>
2024-04-09 16:26:58 +0300

    + docs

--- server.ts
+++ server.ts
@@ -1,38 +1,37 @@
 import express, { type Express } from "express";
 import cors from "cors";
-// import fs from "fs";
-// import path from "path";
-// import morgan from "morgan";
+import passport from "passport";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import * as dotenv from "dotenv";
 import newspostsConfigs from "./routes/newspost";
 import userConfigs from "./routes/user";
 import { errorHandler } from "./services/errorHandler";
-import passport from "passport";
 import { bearerStrategy } from "./middleware/auth-passport";
 import { AppDataSource } from "./db/data-source";
-import * as dotenv from "dotenv";
 import "reflect-metadata";
 
-// const logsDir = path.join(__dirname, "logs");
-// if (!fs.existsSync(logsDir)) {
-//   fs.mkdirSync(logsDir);
-// }
-// export const accessLogStream = fs.createWriteStream(
-//   path.join(logsDir, "access.log"),
-//   {
-//     flags: "a",
-//   }
-// );
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Passport Auth Api",
+      version: "0.0.1",
+    },
+  },
+  apis: ["./routes/*.ts"],
+};
+
+const swaggerDocument = swaggerJSDoc(swaggerOptions);
 
 class Server {
   app: Express;
-  HOST: string;
   PORT: number;
 
   constructor() {
     this.app = express();
+    this.PORT = Number(process.env.PORT) ?? 8000;
     dotenv.config();
-    this.HOST = process.env.HOST ?? "34.123.7.81";
-    this.PORT = Number(process.env.PORT) ?? 9000;
     this.configureMiddleware();
     this.configureRoutes();
     this.configureErrorHandling();
@@ -56,35 +55,17 @@ class Server {
   private configureMiddleware(): void {
     this.app.use(cors());
     passport.use(bearerStrategy);
-    // logs
-    // Потік для виведення журналу у консоль
-    // this.app.use(
-    //   morgan("dev", {
-    //     stream: process.stdout, // Process.stdout для виведення в консоль
-    //   })
-    // );
-    // Логування для morgan
-    // this.app.use(
-    //   morgan(
-    //     (tokens, req, res) => {
-    //       return [
-    //         `HTTP method: ${tokens.method(req, res)}\n`,
-    //         `URL: ${tokens.url(req, res)}\n`,
-    //         `Status: ${tokens.status(req, res)}\n`,
-    //         `Body: ${JSON.stringify(req.body)}\n`,
-    //         // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    //         `Time: ${new Date()}\n`,
-    //       ].join(" ");
-    //     },
-    //     { stream: accessLogStream }
-    //   )
-    // );
   }
 
   private configureRoutes(): void {
     this.app.use("/api/", userConfigs);
     this.app.use("/api/auth/", userConfigs);
     this.app.use("/api/newsposts/", newspostsConfigs);
+    this.app.use(
+      "/api-docs",
+      swaggerUi.serve,
+      swaggerUi.setup(swaggerDocument)
+    );
   }
 
   private configureErrorHandling(): void {
@@ -100,3 +81,44 @@ class Server {
 
 const server = new Server();
 server.start();
+
+// import fs from "fs";
+// import path from "path";
+// import morgan from "morgan";
+//
+// const logsDir = path.join(__dirname, "logs");
+// if (!fs.existsSync(logsDir)) {
+//   fs.mkdirSync(logsDir);
+// }
+// export const accessLogStream = fs.createWriteStream(
+//   path.join(logsDir, "access.log"),
+//   {
+//     flags: "a",
+//   }
+// );
+//
+// middleware
+//
+// logs
+// Потік для виведення журналу у консоль
+// this.app.use(
+//   morgan("dev", {
+//     stream: process.stdout, // Process.stdout для виведення в консоль
+//   })
+// );
+// Логування для morgan
+// this.app.use(
+//   morgan(
+//     (tokens, req, res) => {
+//       return [
+//         `HTTP method: ${tokens.method(req, res)}\n`,
+//         `URL: ${tokens.url(req, res)}\n`,
+//         `Status: ${tokens.status(req, res)}\n`,
+//         `Body: ${JSON.stringify(req.body)}\n`,
+//         // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+//         `Time: ${new Date()}\n`,
+//       ].join(" ");
+//     },
+//     { stream: accessLogStream }
+//   )
+// );
